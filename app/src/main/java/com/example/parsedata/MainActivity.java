cd08960a55f1d50e505879e158f65149fb5e222d
Atiqur Rahman <atiqur.india@gmail.com>
2021-07-17 12:14:44 +0530

    Set up the Volley Network Library and Parse a String.

--- app/src/main/java/com/example/parsedata/MainActivity.java
+++ app/src/main/java/com/example/parsedata/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.parsedata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.VoiceInteractor;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instantiate the RequestQueue
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://www.google.com";
+
+        // Request a string response from the provided URL
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Main", "onResponse: "+ response.substring(0, 500));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error", "onErrorResponse: " + "Failed to get info!");
+            }
+        });
+
+        // Add request to the RequestQueue
+        queue.add(stringRequest);
     }
 }
